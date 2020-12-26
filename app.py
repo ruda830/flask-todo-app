8082d12a03eb42059cdf5836008fadecee0ecba8
ruda <sheimisky7150@yahoo.co.jp>
2020-12-26 09:45:07 +0900

    DBをSQLAlchemyで作った。

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import  SQLAlchemy
+
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+db = SQLAlchemy(app)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(30), nullable=False)
+    detail = db.Column(db.String(100))
+    due = db.Column(db.DateTime, nullable=False)
+
 
 @app.route('/')
 def index():
