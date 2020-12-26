d5e65558d091aa87035b66f26d1616072007445b
ruda <sheimisky7150@yahoo.co.jp>
2020-12-26 16:05:08 +0900

    detailとdeleteを実装。

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template
-from flask_sqlalchemy import  SQLAlchemy
+from datetime import datetime
+
+
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
@@ -14,14 +17,42 @@ class Post(db.Model):
     due = db.Column(db.DateTime, nullable=False)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    if request.method == 'GET':
+        posts = Post.query.all()
+        return render_template('index.html', posts=posts)
+    else:
+        title = request.form.get('title')
+        detail = request.form.get('detail')
+        due = request.form.get('due')
 
+        due = datetime.strptime(due, '%Y-%m-%d')
+        new_post = Post(title=title, detail=detail, due=due)
+
+        db.session.add(new_post)
+        db.session.commit()
+
+        return redirect('/')
 
 @app.route('/create')
 def create():
     return render_template('create.html')
+
+@app.route('/detail/<int:id>')
+def read(id):
+    post = Post.query.get(id)
+    return render_template('detail.html', post=post)
+
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    post = Post.query.get(id)
+    db.session.delete(post)
+    db.session.commit()
+    return redirect('/')
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
